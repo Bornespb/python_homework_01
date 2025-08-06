e02665d26cc096172875e55d26dc4d8046d20001
Aleksei Zharikov <alex_striker@mail.ru>
2025-08-06 15:38:38 +0300

    working prototype

---
+++ log_analyzer/entities.py
@@ -0,0 +1,179 @@
+import re
+from dataclasses import dataclass
+from datetime import datetime
+from statistics import median
+from typing import NamedTuple, TypedDict, Union
+
+
+class JsonConfig(TypedDict):
+    LOG_DIR: str | None
+    REPORT_DIR: str | None
+    REPORT_SIZE: int | None
+    REPORT_TEMPLATE_PATH: str | None
+
+
+@dataclass
+class Config:
+    log_dir: str | None
+    report_dir: str | None
+    report_size: int | None
+    report_template_path: str | None
+
+    @classmethod
+    def from_dict(cls, data: JsonConfig) -> "Config":
+        try:
+            return cls(
+                log_dir=data.get("LOG_DIR"),
+                report_dir=data.get("REPORT_DIR"),
+                report_size=data.get("REPORT_SIZE"),
+                report_template_path=data.get("REPORT_TEMPLATE_PATH"),
+            )
+        except TypeError as e:
+            # structlog?
+            raise e
+
+    @classmethod
+    def get_default_config(cls) -> "Config":
+        return cls(
+            log_dir="./log",
+            report_dir="./report",
+            report_size=1000,
+            report_template_path="./resources/report.html",
+        )
+
+
+@dataclass
+class RawLog:
+    """
+    log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
+                    '$status $body_bytes_sent "$http_referer" '
+                    '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
+                    '$request_time';
+    """
+
+    LOG_FORMAT_TEMPLATE = r"""
+    ^
+    (?P<remote_addr>\S+) \s+                           # $remote_addr
+    (?P<remote_user>\S+) \s+                           # $remote_user
+    (?P<http_x_real_ip>\S+) \s+                        # $http_x_real_ip
+    \[(?P<time_local>[^\]]+)\] \s+                     # [$time_local]
+    "(?P<request>[^"]+)" \s+                           # "$request"
+    (?P<status>\d+) \s+                                # $status
+    (?P<body_bytes_sent>\d+) \s+                       # $body_bytes_sent
+    "(?P<http_referer>[^"]*)" \s+                      # "$http_referer"
+    "(?P<http_user_agent>[^"]*)" \s+                   # "$http_user_agent"
+    "(?P<http_x_forwarded_for>[^"]*)" \s+              # "$http_x_forwarded_for"
+    "(?P<http_X_REQUEST_ID>[^"]*)" \s+                 # "$http_X_REQUEST_ID"
+    "(?P<http_X_RB_USER>[^"]*)" \s+                    # "$http_X_RB_USER"
+    (?P<request_time>[\d.]+)                           # $request_time
+    $
+"""
+
+    remote_addr: str
+    remote_user: str
+    http_x_real_ip: str
+    time_local: str
+    request: str
+    status: int
+    body_bytes_sent: int
+    http_referer: str
+    http_user_agent: str
+    http_x_forwarded_for: str
+    http_X_REQUEST_ID: str
+    http_X_RB_USER: str
+    request_time: float
+
+    @classmethod
+    def from_str(cls, data: str) -> "RawLog":
+        try:
+            log_fields = re.match(cls.LOG_FORMAT_TEMPLATE, data, re.VERBOSE)
+            if not log_fields:
+                raise ValueError(f"Invalid log format: {data}")
+            log_fields = log_fields.groupdict()  # type: ignore
+            return cls(
+                remote_addr=log_fields["remote_addr"],
+                remote_user=log_fields["remote_user"],
+                http_x_real_ip=log_fields["http_x_real_ip"],
+                time_local=log_fields["time_local"],
+                request=log_fields["request"],
+                status=int(log_fields["status"]),
+                body_bytes_sent=int(log_fields["body_bytes_sent"]),
+                http_referer=log_fields["http_referer"],
+                http_user_agent=log_fields["http_user_agent"],
+                http_x_forwarded_for=log_fields["http_x_forwarded_for"],
+                http_X_REQUEST_ID=log_fields["http_X_REQUEST_ID"],
+                http_X_RB_USER=log_fields["http_X_RB_USER"],
+                request_time=float(log_fields["request_time"]),
+            )
+        except (TypeError, ValueError) as e:
+            raise e
+
+
+@dataclass
+class UriStatistics:
+    uri: str
+    log_count: int
+    log_count_perc: float
+    time_sum: float
+    time_perc: float
+    time_avg: float
+    time_max: float
+    request_times: list[float]
+
+    @property
+    def time_med(self) -> float:
+        return median(self.request_times)
+
+    def to_dict(self) -> dict[str, str | int]:
+        return {
+            "uri": self.uri,
+            "log_count": self.log_count,
+            "log_count_perc": f"{self.log_count_perc:.3f}",
+            "time_sum": f"{self.time_sum:.3f}",
+            "time_perc": f"{self.time_perc:.3f}",
+            "time_avg": f"{self.time_avg:.3f}",
+            "time_max": f"{self.time_max:.3f}",
+            "time_med": f"{self.time_med:.3f}",
+        }
+
+    @classmethod
+    def update_statistics(
+        cls,
+        statistics: Union["UriStatistics", None],
+        log_entry: RawLog,
+        total_count: int,
+        total_request_time: float,
+    ) -> tuple[bool, Union["UriStatistics", None]]:
+        try:
+            if not statistics:
+                return True, cls(
+                    uri=log_entry.request.split()[1],
+                    log_count=1,
+                    log_count_perc=(1 / total_count) * 100,
+                    time_sum=log_entry.request_time,
+                    time_perc=(log_entry.request_time / total_request_time) * 100,
+                    time_avg=log_entry.request_time,
+                    time_max=log_entry.request_time,
+                    request_times=[log_entry.request_time],
+                )
+            else:
+                statistics.log_count += 1
+                statistics.log_count_perc = (statistics.log_count / total_count) * 100
+                statistics.time_sum += log_entry.request_time
+                statistics.time_perc = (statistics.time_sum / total_request_time) * 100
+                statistics.time_avg = statistics.time_sum / statistics.log_count
+                statistics.time_max = (
+                    statistics.time_max
+                    if statistics.time_max > log_entry.request_time
+                    else log_entry.request_time
+                )
+                statistics.request_times.append(log_entry.request_time)
+                return True, statistics
+        except Exception:
+            return False, None
+
+
+class LogInfo(NamedTuple):
+    log_date: datetime
+    file_name: str
+    file_ext: str | None
---
+++ log_analyzer/log_analyzer.py
@@ -0,0 +1,131 @@
+# log_format ui_short '$remote_addr  $remote_user $http_x_real_ip [$time_local] "$request" '
+#                     '$status $body_bytes_sent "$http_referer" '
+#                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
+#                     '$request_time';
+
+import argparse
+import gzip
+import json
+import os
+import re
+import sys
+from collections.abc import Generator
+from datetime import datetime
+from string import Template
+
+from entities import Config, JsonConfig, LogInfo, RawLog, UriStatistics
+
+LOG_FILES_PATTERN = r"nginx-access-ui.log-(\d{8})(?:\.gz)?$"
+REPORT_FILE_NAME_TEMPLATE = "report-$year.$month.$day.html"
+
+
+def parse_config() -> str | None:
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument("--config", help="allow specifying custom configuration file", type=str)
+    args = args_parser.parse_args()
+    return args.config if args.config else None
+
+
+def merge_config(default_config: Config, external_config_path: str | None) -> Config:
+    if not external_config_path:
+        return default_config
+
+    with open(external_config_path) as config_file:
+        config_data = json.load(config_file)
+
+    external_config = Config.from_dict(JsonConfig(config_data))
+    return Config(
+        log_dir=external_config.log_dir or default_config.log_dir,
+        report_dir=external_config.report_dir or default_config.report_dir,
+        report_size=external_config.report_size or default_config.report_size,
+        report_template_path=external_config.report_template_path or default_config.report_template_path,
+    )
+
+
+def get_last_log_file_name(log_folder_path: str) -> LogInfo:
+    latest_log_file = None
+    latest_log_date = None
+
+    for file in os.scandir(log_folder_path):
+        file_match = re.match(LOG_FILES_PATTERN, file.name)
+        if not file_match:
+            continue
+        current_date = file_match.group(1)
+        if latest_log_date is None or current_date > latest_log_date:
+            latest_log_date = current_date
+            latest_log_file = file.path
+
+    if not latest_log_date or not latest_log_file:
+        raise ValueError(f"No log files provided in the {log_folder_path} folder.")
+
+    _, file_ext = os.path.splitext(latest_log_file)
+
+    return LogInfo(
+        log_date=datetime.strptime(latest_log_date, "%Y%m%d"),
+        file_name=latest_log_file,
+        file_ext=file_ext,
+    )
+
+
+def read_log_file(log_file_path: str, log_file_ext: str) -> Generator[str]:
+    with gzip.open(log_file_path, "rt") if log_file_ext == ".gz" else open(log_file_path) as log_file:
+        yield from log_file.readlines()
+
+
+def analyze_logs(log_line_gen: Generator[str]) -> list[UriStatistics]:
+    report_data: dict[str, UriStatistics] = {}
+    total_line_count = 0
+    total_request_time = 0.0
+    failed_logs = 0
+    for log_line in log_line_gen:
+        log_entry = RawLog.from_str(log_line)
+        total_line_count += 1
+        total_request_time += log_entry.request_time
+        report_entry = report_data.get(log_entry.request, None)
+        is_updated, report_entry = UriStatistics.update_statistics(
+            report_entry, log_entry, total_line_count, total_request_time
+        )
+        if not is_updated:
+            failed_logs += 1
+            continue
+        report_data[log_entry.request] = report_entry
+    return list(report_data.values())
+
+
+def generate_report(log_date: datetime, processed_logs: list[UriStatistics], config: Config) -> None:
+    filtered_logs = list(filter(lambda x: x.time_sum > config.report_size, processed_logs))
+    sorted_logs = sorted(filtered_logs, key=lambda x: x.time_sum, reverse=True)
+    json_logs = json.dumps([log.to_dict() for log in sorted_logs])
+
+    with open(config.report_template_path) as report_template:
+        report_file_template = report_template.read()
+    template = Template(report_file_template)
+
+    file_content = template.safe_substitute(table_json=json_logs)
+
+    report_file_name = Template(REPORT_FILE_NAME_TEMPLATE).substitute(
+        year=log_date.year, month=log_date.month, day=log_date.day
+    )
+
+    report_path = os.path.join(config.report_dir, report_file_name)
+    with open(report_path, "w") as report_file:
+        report_file.write(file_content)
+
+
+def main() -> None:
+    try:
+        config = Config.get_default_config()
+        args_config_path = parse_config()
+        config = merge_config(config, args_config_path)
+
+        last_log_info = get_last_log_file_name(config.log_dir)
+
+        processed_logs = analyze_logs(read_log_file(last_log_info.file_name, last_log_info.file_ext))
+        generate_report(last_log_info.log_date, processed_logs, config)
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
